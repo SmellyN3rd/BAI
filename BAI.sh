2bd3f721bb16d2d4af0689d34fd4c0010657a24a
Miłosz Kusz <70511617+SmellyN3rd@users.noreply.github.com>
2021-03-16 18:50:27 +0100

    Update BAI.sh

--- BAI.sh
+++ BAI.sh
@@ -33,8 +33,8 @@ mkswap $DRIVE\1 -L swap
 mount $DRIVE\2 /mnt
 swapon $DRIVE\1
 
-pacstrap /mnt base base-devel linux-lts linux-lts-firmware nano dhcpcd netctl
+pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
 
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime && hwclock --systohc && echo archbox > /etc/hostname && echo "pl_PL.UTF-8 UTF-8" >> /etc/locale.gen && echo 'LANG="pl_PL.UTF-8"' > /etc/locale.conf && locale-gen && echo KEYMAP=de-latin1 > /etc/vconsole.conf && mkinitcpio -p linux-lts && echo '[multilib]' >> /etc/pacman.conf && echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf && pacman -Syy && pacman -S dialog wpa_supplicant --noconfirm  && pacman -S grub --noconfirm  && PART=$(df / | grep / | cut -d" " -f1) && grub-install $PART && grub-mkconfig -o /boot/grub/grub.cfg && clear && echo enter root password && passwd && read -p "enter your username: " username && useradd -m -g users -G wheel -s /bin/bash $username && echo enter password for the new user && passwd $username && echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers && reboot
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime && hwclock --systohc && echo archbox > /etc/hostname && echo "pl_PL.UTF-8 UTF-8" >> /etc/locale.gen && echo 'LANG="pl_PL.UTF-8"' > /etc/locale.conf && locale-gen && echo KEYMAP=de-latin1 > /etc/vconsole.conf && mkinitcpio -p linux && echo '[multilib]' >> /etc/pacman.conf && echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf && pacman -Syy && pacman -S dialog wpa_supplicant --noconfirm  && pacman -S grub --noconfirm  && PART=$(df / | grep / | cut -d" " -f1) && grub-install $PART && grub-mkconfig -o /boot/grub/grub.cfg && clear && echo enter root password && passwd && read -p "enter your username: " username && useradd -m -g users -G wheel -s /bin/bash $username && echo enter password for the new user && passwd $username && echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers && reboot
