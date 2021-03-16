e3aca4eda23a16618145018562551025d353bf7d
Miłosz Kusz <70511617+SmellyN3rd@users.noreply.github.com>
2021-03-16 17:54:13 +0100

    Update BAI.sh

--- BAI.sh
+++ BAI.sh
@@ -22,3 +22,13 @@ SWAPSIZE=+$(grep MemTotal /proc/meminfo | awk '{print $2 / 900000}')G
 
   echo w;
 ) | fdisk $DRIVE
+
+mkfs.ext4  $DRIVE\2 -L root
+mkswap $DRIVE\1 -L swap
+
+mount $DRIVE\2 /mnt
+swapon $DRIVE\1
+
+pacstrap /mnt base base-devel linux-lts linux-firmware nano dhcpcd netctl
+
+genfstab -pU /mnt >> /mnt/etc/fstab
