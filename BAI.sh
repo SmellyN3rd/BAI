97bb5c77e38fa28477bbdc5e03eb42726dc7c8fd
Miłosz Kusz <70511617+SmellyN3rd@users.noreply.github.com>
2021-03-16 17:24:17 +0100

    Add files via upload

---
+++ BAI.sh
@@ -0,0 +1,27 @@
+lsblk -do NAME,SIZE
+echo 
+read -p 'Pick the drive to install to: ' DRIVE
+DRIVE=/dev/$DRIVE
+
+SWAPSIZE=$(grep MemTotal /proc/meminfo | awk '{print $2 / 900000}')G
+
+clear
+
+(
+  echo o;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo +$SWAPSIZE;
+
+  echo n;
+  echo p;
+  echo;
+  echo;
+  echo a;
+  echo 1;
+
+  echo w;
+) | fdisk $DRIVE
