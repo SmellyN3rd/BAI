9a21a53a5f10b3c8af1f8fc8d4471f5b0f4b54ba
Miłosz Kusz <70511617+SmellyN3rd@users.noreply.github.com>
2021-03-17 12:18:47 +0100

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -1,26 +1,27 @@
 ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime 
-hwclock --systohc 
 echo archbox > /etc/hostname 
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen 
 echo "pl_PL.UTF-8 UTF-8" >> /etc/locale.gen 
 locale-gen 
 echo 'LANG="pl_PL.UTF-8"' > /etc/locale.conf 
+mkinitcpio -p linux-lts
 echo KEYMAP=pl > /etc/vconsole.conf 
 echo FONT=Lat2-Terminus16.psfu.gz >> /etc/vconsole.conf 
 echo FONT_MAP=8859-2 >> /etc/vconsole.conf 
 echo '[multilib]' >> /etc/pacman.conf 
 echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf 
 pacman -Syy 
-pacman -S dialog wpa_supplicant networkmanager network-manager-applet ppp --noconfirm  
-pacman -S grub --noconfirm  
-mkinitcpio -p linux-lts
+pacman -S grub dialog wpa_supplicant networkmanager network-manager-applet ppp --noconfirm  
 grub-install --target=x86_64 --recheck
+sleep 10
 grub-mkconfig -o /boot/grub/grub.cfg 
+sleep 10
 clear 
 echo enter root password 
-passwd su
+passwd
 read -p "enter your username: " username 
 useradd -m -g users -G wheel,storage,power -s /bin/bash $username 
-echo enter password for the new user 
+echo enter password for the user $username
 passwd $username 
 echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers 
 systemctl enable NetworkManager
