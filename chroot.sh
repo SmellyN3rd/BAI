2910bc4feafd98ae282e720986cab9da9bd614ee
Miłosz Kusz <70511617+SmellyN3rd@users.noreply.github.com>
2021-03-17 11:52:03 +0100

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -1,31 +1,27 @@
-if [ "$(stat -c %d:%i /)" != "$(stat -c %d:%i /proc/1/root/.)" ]; then
-  echo "We are chrooted!"
-else
-  echo "Business as usual"
-fi
-
-sleep 10
 ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime 
 hwclock --systohc 
 echo archbox > /etc/hostname 
 echo "pl_PL.UTF-8 UTF-8" >> /etc/locale.gen 
-echo 'LANG="pl_PL.UTF-8"' > /etc/locale.conf 
 locale-gen 
-echo KEYMAP=de-latin1 > /etc/vconsole.conf 
+echo 'LANG="pl_PL.UTF-8"' > /etc/locale.conf 
+echo KEYMAP=pl > /etc/vconsole.conf 
+echo FONT=Lat2-Terminus16.psfu.gz >> /etc/vconsole.conf 
+echo FONT_MAP=8859-2 >> /etc/vconsole.conf 
 echo '[multilib]' >> /etc/pacman.conf 
 echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf 
 pacman -Syy 
 pacman -S dialog wpa_supplicant networkmanager network-manager-applet ppp --noconfirm  
 pacman -S grub --noconfirm  
 mkinitcpio -p linux-lts
-grub-install
+grub-install --target=x86_64 --recheck
 grub-mkconfig -o /boot/grub/grub.cfg 
 clear 
 echo enter root password 
-passwd 
+passwd su
 read -p "enter your username: " username 
-useradd -m -g users -G wheel -s /bin/bash $username 
+useradd -m -g users -G wheel,storage,power -s /bin/bash $username 
 echo enter password for the new user 
 passwd $username 
 echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers 
 systemctl enable NetworkManager
+exit
