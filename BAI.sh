6bd90e3e0e2d16dd3b292f5a76137c6da1d189d3
Miłosz Kusz <70511617+SmellyN3rd@users.noreply.github.com>
2021-03-17 11:41:17 +0100

    Update BAI.sh

--- BAI.sh
+++ BAI.sh
@@ -33,7 +33,7 @@ mkswap $DRIVE\1 -L swap
 mount $DRIVE\2 /mnt
 swapon $DRIVE\1
 
-pacstrap /mnt base base-devel linux-lts linux-firmware nano dhcpcd netctl
+pacstrap /mnt base base-devel linux-lts linux-firmware dhcpcd netctl
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
