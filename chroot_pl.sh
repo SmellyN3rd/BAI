072aa066f47a6d87e7fd6b5df0b5052fea436ed0
Miłosz Kusz <70511617+SmellyN3rd@users.noreply.github.com>
2021-03-21 09:17:50 +0100

    Add files via upload

---
+++ chroot_pl.sh
@@ -0,0 +1,37 @@
+ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime 
+
+echo archbox > /etc/hostname 
+
+echo "pl_PL.UTF-8 UTF-8" >> /etc/locale.gen 
+locale-gen 
+echo 'LANG="pl_PL.UTF-8"' > /etc/locale.conf 
+
+mkinitcpio -p linux-lts
+
+echo KEYMAP=pl > /etc/vconsole.conf 
+echo FONT=Lat2-Terminus16.psfu.gz >> /etc/vconsole.conf 
+echo FONT_MAP=8859-2 >> /etc/vconsole.conf 
+
+echo '[multilib]' >> /etc/pacman.conf 
+echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf 
+pacman -Syy 
+
+pacman -S grub wpa_supplicant networkmanager network-manager-applet --noconfirm  
+
+grub-install --recheck $(<drive.tmp)
+grub-mkconfig -o /boot/grub/grub.cfg 
+
+rm drive.tmp
+clear 
+
+echo enter root password 
+passwd
+#echo
+#read -p "enter your username: " username 
+#useradd -m -g users -G wheel,storage,power -s /bin/bash $username 
+#echo enter password for the user $username
+#passwd $username 
+echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers 
+
+systemctl enable NetworkManager
+exit
