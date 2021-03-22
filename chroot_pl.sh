fc6476d1e2a9a1b236b5c31c95d2522965f8e116
Miłosz Kusz <70511617+SmellyN3rd@users.noreply.github.com>
2021-03-22 12:21:16 +0100

    Update chroot_pl.sh

--- chroot_pl.sh
+++ chroot_pl.sh
@@ -1,38 +1,24 @@
-ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime 
+echo -ne setting timezone... && ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime && echo done
 
-echo archbox > /etc/hostname 
+echo -ne setting hostname... && echo archbox > /etc/hostname && echo done
 
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen 
-echo "pl_PL.UTF-8 UTF-8" >> /etc/locale.gen 
-locale-gen 
-echo 'LANG="pl_PL.UTF-8"' > /etc/locale.conf 
+echo -ne generating locales... && echo "en_US.UTF-8 UTF-8" > /etc/locale.gen && echo "pl_PL.UTF-8 UTF-8" >> /etc/locale.gen && locale-gen > /dev/null && echo 'LANG="pl_PL.UTF-8"' > /etc/locale.conf && echo done
 
-mkinitcpio -p linux-lts
+echo -ne running mkinitcpio... && mkinitcpio -p linux-lts > /dev/null && echo done
 
-echo KEYMAP=pl > /etc/vconsole.conf 
-echo FONT=Lat2-Terminus16.psfu.gz >> /etc/vconsole.conf 
-echo FONT_MAP=8859-2 >> /etc/vconsole.conf 
+echo -ne configuring vconsole... && echo KEYMAP=pl > /etc/vconsole.conf && echo FONT=Lat2-Terminus16.psfu.gz >> /etc/vconsole.conf && echo FONT_MAP=8859-2 >> /etc/vconsole.conf && echo done
 
-echo '[multilib]' >> /etc/pacman.conf 
-echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf 
-pacman -Syy 
+echo -ne configuring pacman and the mirrorlist... && echo '[multilib]' >> /etc/pacman.conf && echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf && reflector --country US --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist > /dev/null && pacman -Syy > /dev/null && echo done 
 
-pacman -S grub wpa_supplicant networkmanager network-manager-applet --noconfirm  
+echo -ne configuring networking utilities... && pacman --noconfirm -S wpa_supplicant networkmanager network-manager-applet > /dev/null && systemctl enable NetworkManager > /dev/null && echo done
 
-grub-install --recheck $(<drive.tmp)
-grub-mkconfig -o /boot/grub/grub.cfg 
+echo -ne installing bootloader... && pacman --noconfirm -S grub > /dev/null && grub-install --recheck $(<drive.tmp) > /dev/null && grub-mkconfig -o /boot/grub/grub.cfg >> /dev/null && rm drive.tmp && echo done
 
-rm drive.tmp
-clear 
+echo -ne configuring the sudoers file... && echo '%wheel ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers && echo done
 
+echo
 echo enter root password 
 passwd
-#echo
-#read -p "enter your username: " username 
-#useradd -m -g users -G wheel,storage,power -s /bin/bash $username 
-#echo enter password for the user $username
-#passwd $username 
-echo '%wheel ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers 
 
-systemctl enable NetworkManager
+echo installation complete, have fun :)
 exit
