93ef3d570ba260f20e625a4390af9fbb45087cd3
Miłosz Kusz <70511617+SmellyN3rd@users.noreply.github.com>
2021-03-24 19:30:02 +0100

    Update BAI.sh

--- BAI.sh
+++ BAI.sh
@@ -23,19 +23,30 @@ echo -ne partitioning the drive... && dd if=/dev/zero of=$DRIVE bs=512 count=1 c
   echo;
 
   echo w;
-) | fdisk $DRIVE &> /dev/null && echo done
-
-echo -ne creating file systems... && mkfs.ext4  $DRIVE\2 -L root &> /dev/null && mkswap $DRIVE\1 -L swap &> /dev/null && echo done
-
-echo -ne mounting partitions... && mount $DRIVE\2 /mnt &> /dev/null && swapon $DRIVE\1 &> /dev/null && echo done
-
-echo -ne downloading needed files and creating the file structure... && pacstrap /mnt base base-devel linux-lts linux-firmware reflector &> /dev/null && echo done
-
-echo -ne generating fstab... && genfstab -U -p /mnt >> /mnt/etc/fstab && echo done
+) | fdisk $DRIVE &> /dev/null 
 echo $DRIVE > /mnt/drive.tmp
+echo done
+
+echo -ne creating file systems... 
+mkfs.ext4  $DRIVE\2 -L root &> /dev/null 
+mkswap $DRIVE\1 -L swap &> /dev/null 
+echo done
+
+echo -ne mounting partitions... 
+mount $DRIVE\2 /mnt &> /dev/null 
+swapon $DRIVE\1 &> /dev/null 
+echo done
+
+echo -ne downloading needed files and creating the file structure... 
+pacstrap /mnt base base-devel linux-lts linux-firmware reflector &> /dev/null 
+echo done
+
+echo -ne generating fstab... 
+genfstab -U -p /mnt >> /mnt/etc/fstab 
+echo done
 
 echo
 read -p 'choose your language (available: en, pl): ' LANG
-
 arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/SmellyN3rd/BAI/main/chroot_$LANG.sh)"
+
 reboot
